7f6a2398576120c1c8f4c843ccd32b8c795b71ba
Rafael Mitsunaka de Andrade <rafandrade@prestadores.amil.com.br>
2017-04-19 18:49:33 -0300

    Commit

---
+++ Financas/Controllers/LoginController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebMatrix.WebData;
+
+namespace Financas.Controllers
+{
+    public class LoginController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Autentica(string login, string senha)
+        {
+            if (WebSecurity.Login(login, senha))
+                return RedirectToAction("Index", "Movimentacao");
+            else { 
+                ModelState.AddModelError("login.Invalido", "Login ou senha incorretos");
+                return View("Index");
+            }
+        }
+
+        public ActionResult Logout()
+        {
+            WebSecurity.Logout();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Financas/DAO/UsuarioDAO.cs
@@ -0,0 +1,27 @@
+﻿using Financas.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Financas.DAO
+{
+    public class UsuarioDAO
+    {
+        private FinancasContext _context;
+        public UsuarioDAO(FinancasContext context)
+        {
+            _context = context;
+        }
+        public void Adiciona(Usuario usuario)
+        {
+            _context.Usuarios.Add(usuario);
+            _context.SaveChanges();
+        }
+
+        public IList<Usuario> Lista()
+        {
+            return _context.Usuarios.ToList();
+        }
+    }
+}
